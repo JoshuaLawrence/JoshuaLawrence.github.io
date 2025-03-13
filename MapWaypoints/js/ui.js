47bdb190530c368fe8bd5c8b5505a8f8e2293cdb
JoshuaLawrence <xusies@live.com>
2025-03-13 17:32:47 +1030

    fixed the root index name, added tileLayer to leaflet map

--- MapWaypoints/js/ui.js
+++ MapWaypoints/js/ui.js
@@ -1,5 +1,12 @@
 
 
 window.onload = function() {
-    var map = L.map('map').setView([-34.98322204585383, 138.5783569753705],13);
+    var map = L.map('map',{
+        center: [-34.98322204585383, 138.5783569753705],
+        zoom:   15    
+    });
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
 }
