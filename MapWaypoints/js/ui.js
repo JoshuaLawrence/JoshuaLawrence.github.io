67abfb68f8955235107b2498d36103f2b0ee6514
JoshuaLawrence <xusies@live.com>
2025-03-13 18:14:12 +1030

    feat: added an alert box with optional accept/reject callbacks

--- MapWaypoints/js/ui.js
+++ MapWaypoints/js/ui.js
@@ -1,6 +1,7 @@
 
 
 window.onload = function() {
+    createAlert("Hello World");
     var map = L.map('map',{
         center: [-34.98322204585383, 138.5783569753705],
         zoom:   15    
@@ -9,4 +10,62 @@ window.onload = function() {
         maxZoom: 19,
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(map);
+}
+
+function createAlert(msg,acceptText = null,rejectText = null,acceptCallback = null, rejectCallback = null){
+    //set the alert message
+    alertMsg.innerHtml = msg;
+
+    //set accept text - default to Ok
+    if(acceptText != null){
+        alertAccept.innerHtml = acceptText;
+    }else{
+        alertAccept.innerHtml = "Ok";
+    }
+
+    //set accept callback
+    if(acceptCallback != null){
+        alertAccept.addEventListener("click",()=>{
+            acceptCallback();
+            closeAlert();
+        });
+    }else{
+        alertAccept.addEventListener("click",()=>{
+            closeAlert();
+        });
+    }
+
+    //reject button - not neccessarily always visible
+    if(rejectCallback != null || rejectText != null){
+        //set the reject button text
+        if(rejectText != null){
+            alertReject.innerHtml = rejectText;
+        }else{
+            alertReject.innerHtml = "Cancel";
+        }
+        //show the reject button
+        alertReject.style.display = "";
+        //set the reject button callback
+        if(rejectCallback != null){
+            alertReject.addEventListener("click",()=>{
+                rejectCallback();
+                closeAlert();
+            });
+        }else{
+            alertReject.addEventListener("click",()=>{
+                closeAlert();
+            });
+        }
+    }
+
+    //show the alert container
+    alertContainer.style.display = "";
+}
+function closeAlert(){
+    alertContainer.style.display = "none";
+    alertAccept.removeEventListener("click");
+    alertReject.removeEventListener("click");
+    alertAccept.innerHtml = "";
+    alertReject.innerHtml = "";
+    alertReject.style.display = "none";
 }
