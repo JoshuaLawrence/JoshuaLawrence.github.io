2e8e87cc70bcb0c3fcfaead9fcc0303c65ea1008
JoshuaLawrence <xusies@live.com>
2025-03-14 12:11:30 +1030

    feat: added service worker

--- AoSReminder/js/ui.js
+++ AoSReminder/js/ui.js
@@ -50,6 +50,8 @@ var lastXPosition;
 document.addEventListener("DOMContentLoaded", init);
 
 async function init(){
+    await registerServiceWorker();
+
     await dbSetup();
 
 
@@ -81,6 +83,98 @@ async function init(){
 
     pageTitle.innerHTML = PAGES.getPageName(currentPage);
 
+}
+
+//service worker
+const registerServiceWorker = async () => {
+    if ("serviceWorker" in navigator) {
+        try {
+            const registration = await navigator.serviceWorker.register("/AoSReminder/sw.js", {
+                scope: "/AoSReminder/",
+            });
+            if (registration.installing) {
+                console.log("Service worker installing");
+            } else if (registration.waiting) {
+                console.log("Service worker installed");
+            } else if (registration.active) {
+                console.log("Service worker active");
+            }
+        } catch (error) {
+            console.error(`Registration failed with ${error}`);
+        }
+    }
+};
+
+let alertQueue = [];
+function createAlert(msg,acceptText = null,rejectText = null,acceptCallback = null, rejectCallback = null){
+    //add to alert queue if alert box is already open
+    if(alertContainer.style.display != "none"){
+        alertQueue.push([msg,acceptText,rejectText,acceptCallback,rejectCallback]);
+        return;
+    }
+
+    //set the alert message
+    alertMsg.innerText = msg;
+
+    //set accept text - default to Ok
+    if(acceptText != null){
+        alertAccept.innerText = acceptText;
+    }else{
+        alertAccept.innerText = "Ok";
+    }
+
+    //set accept callback
+    if(acceptCallback != null){
+        alertAccept.onclick = () => {
+            acceptCallback();
+            closeAlert();
+        };
+    }else{
+        alertAccept.onclick = () => {
+            closeAlert();
+        };
+    }
+
+    //reject button - not neccessarily always visible
+    if(rejectCallback != null || rejectText != null){
+        //set the reject button text
+        if(rejectText != null){
+            alertReject.innerText = rejectText;
+        }else{
+            alertReject.innerText = "Cancel";
+        }
+        //show the reject button
+        alertReject.style.display = "";
+        //set the reject button callback
+        if(rejectCallback != null){
+            alertReject.onclick = () => {
+                rejectCallback();
+                closeAlert();
+            };
+        }else{
+            alertReject.onclick = () => {
+                closeAlert();
+            };
+        }
+    }
+
+    //show the alert container
+    alertContainer.style.display = "";
+}
+function closeAlert(){
+    alertContainer.style.display = "none";
+    alertAccept.onclick = "";
+    alertReject.onclick = "";
+    alertAccept.innerHtml = "";
+    alertReject.innerHtml = "";
+    alertReject.style.display = "none";
+    //show the next queued alert
+    if(alertQueue.length > 0){
+        let alertParams = alertQueue.shift();
+        setTimeout(()=>{
+            createAlert(...alertParams);
+        },500);
+    }
 }
 
 function handleScrollBtn(event){
